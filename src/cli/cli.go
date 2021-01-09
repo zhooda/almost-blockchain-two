9ef023e4e6e0950620d522d5a5717a2b0c3b1c8d
Zeeshan Hooda <zeeshanhooda@icloud.com>
2021-01-09 13:37:07 -0700

    refactor: moved cli logic to cli package

---
+++ src/cli/cli.go
@@ -0,0 +1,152 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+
+	"github.com/zhooda/almost-blockchain-two/blockchain"
+)
+
+// CommandLine should not be an exported
+// type, I just like the naming convention
+// and don't want the go compiler to get mad
+// so I'm adding this comment
+type CommandLine struct{}
+
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  get-balance -address ADDRESS - get the balance for an address")
+	fmt.Println("  create-chain -address ADDRESS - creates a blockchain")
+	fmt.Println("  print - prints the blocks in the chain")
+	fmt.Println("  send -from FROM -to TO -amount AMOUNT - send amount of tokens to an address")
+}
+
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+func (cli *CommandLine) printChain() {
+	chain := blockchain.ContinueBlockChain("")
+	defer chain.Database.Close()
+	iter := chain.Iterator()
+
+	for {
+		block := iter.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevHash)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
+}
+
+func (cli *CommandLine) getBalance(address string) {
+	chain := blockchain.ContinueBlockChain(address)
+	defer chain.Database.Close()
+
+	balance := 0
+	UTXOs := chain.FindUTXO(address)
+
+	for _, out := range UTXOs {
+		balance += out.Value
+	}
+
+	fmt.Printf("balance of %s: %d dhicoins\n", address, balance)
+}
+
+func (cli *CommandLine) send(from, to string, amount int) {
+	chain := blockchain.ContinueBlockChain(from)
+	defer chain.Database.Close()
+
+	tx := blockchain.NewTransaction(from, to, amount, chain)
+	chain.AddBlock([]*blockchain.Transaction{tx})
+	fmt.Println("success!")
+}
+
+func (cli *CommandLine) createBlockChain(address string) {
+	chain := blockchain.InitBlockChain(address)
+	chain.Database.Close()
+	fmt.Println("finished!")
+}
+
+// Run will run through all of the command line logic
+func (cli *CommandLine) Run() {
+	cli.validateArgs()
+
+	getBalanceCmd := flag.NewFlagSet("get-balance", flag.ExitOnError)
+	createBlockchainCmd := flag.NewFlagSet("create-chain", flag.ExitOnError)
+	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print-chain", flag.ExitOnError)
+
+	getBalanceAddress := getBalanceCmd.String("address", "", "the address to get balance for")
+	createBlockchainAddress := createBlockchainCmd.String("address", "", "the address to send the genesis block reward to")
+	sendFrom := sendCmd.String("from", "", "source wallet address")
+	sendTo := sendCmd.String("to", "", "destination wallet address")
+	sendAmount := sendCmd.Int("amount", 0, "amount to send")
+
+	switch os.Args[1] {
+	case "get-balance":
+		err := getBalanceCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "create-chain":
+		err := createBlockchainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "print":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "send":
+		err := sendCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+
+	if getBalanceCmd.Parsed() {
+		if *getBalanceAddress == "" {
+			getBalanceCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.getBalance(*getBalanceAddress)
+	}
+
+	if createBlockchainCmd.Parsed() {
+		if *createBlockchainAddress == "" {
+			createBlockchainCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.createBlockChain(*createBlockchainAddress)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+
+	if sendCmd.Parsed() {
+		if *sendFrom == "" || *sendTo == "" || *sendAmount <= 0 {
+			sendCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.send(*sendFrom, *sendTo, *sendAmount)
+	}
+}
